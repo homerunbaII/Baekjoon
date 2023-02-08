c9b7c96b8e313d8053d1132a5868893eab8cd913
homerunbaII <thomas3737@korea.ac.kr>
2023-02-08 10:50:45 +0900

    0208

---
+++ python/기본 수학1/2275.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for i in range(t):
+    k = int(input())
+    n = int(input())
+    num = [i for i in range(1, n + 1)]
+
+    for x in range(k):
+        for j in range(1, n):
+            num[j] += num[j - 1]
+    print(num[-1])
