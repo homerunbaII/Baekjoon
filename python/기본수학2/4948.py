e20282fc442c46cbf3cd857fbd3e137748b74803
homerunbaII <thomas3737@korea.ac.kr>
2023-02-15 15:40:03 +0900

    0215

--- python/기본수학2/4948.py
+++ python/기본수학2/4948.py
@@ -1,4 +1,4 @@
-def find_primenumber(num):
+def find_primenumber(num):  # 소수를 판별하는 메서드
     if (num == 1):
         return 0
     for i in range(2, num):
@@ -7,7 +7,7 @@ def find_primenumber(num):
     return num
 
 
-def find_primenumber2(num, list):
+def find_primenumber2(num, list):  # 아르토스테네스의 체
     if (num == 1):
         return 0
     for i in list:
@@ -19,23 +19,23 @@ def find_primenumber2(num, list):
     return num
 
 
-primenum_list = []
+primenum_list = []  # 루트n까지의 소수 리스트
 
 for i in range(1, int(246912 ** (1/2)) + 1):
     if (find_primenumber(i) > 0):
         primenum_list.append(i)
 
 fianl_primenum_list = []
-for i in range(1, 246912):
+for i in range(1, 246912):  # 아리토스테네스의 체
     if (find_primenumber2(i, primenum_list) > 0):
-        fianl_primenum_list.append(i)
+        fianl_primenum_list.append(i)  # 체로 걸러진 1~ 246912까지의 소수
 
 n = int(input())
 
 while (n):
     cnt = 0
     for i in fianl_primenum_list:
-        if n < i <= 2*n:
+        if n < i <= 2*n:  # 범위 내에 있으면 카운트 + 1
             cnt += 1
 
     print(cnt)
---
+++ python/기본수학2/9020.py
@@ -0,0 +1,39 @@
+def find_primenumber(num):  # 소수 찾는 메서드
+    if (num == 1):
+        return 0
+    for i in range(2, int(num ** 0.5)):
+        if (num % i == 0):
+            return 0
+    return num
+
+
+def find_nearnum(num, list):  # 리스트 내 가장 근사한 값을 찾는 메서드
+    while (True):
+        for i in list:
+            if (i == num):
+                return num
+        num -= 1
+
+
+primenumber_list = []
+
+for i in range(1, 10001):  # 1 ~~ 10000까지 소수 리스트 구하기
+    if (find_primenumber(i) > 0):
+        primenumber_list.append(i)
+
+t = int(input())
+
+for i in range(t):
+    num = int(input())  # num 입력받기
+    p1 = int(num / 2)  # num 2로 나누기
+    # 2로 나눈 값 primenumber_list에서 근사소수값 찾기
+    middle_primenum_1 = find_nearnum(p1, primenumber_list)
+    location = primenumber_list.index(middle_primenum_1)  # 근사소수값의 인덱스 위치 반환
+    while (True):
+        middle_primenum_2 = num - middle_primenum_1  # 소수
+        if middle_primenum_2 in primenumber_list:
+            print(middle_primenum_1, middle_primenum_2)
+            break
+        else:
+            location -= 1  # 인덱스 값 1 감소
+            middle_primenum_1 = primenumber_list[location]  # 세로운 중앙 소수값 대입
