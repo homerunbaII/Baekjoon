e5a5524dab9b76a6f0eeb1614d2d817d57e4ec29
homerunbaII <thomas3737@korea.ac.kr>
2023-03-05 16:58:58 +0900

    0305

---
+++ python/알고리즘/자료구조/큐/2164.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+n = int(input())
+
+que = deque([i for i in range(1, n + 1)])
+
+while (len(que) != 1):
+    que.popleft()
+    que.append(que.popleft())
+
+
+print(que[0])
