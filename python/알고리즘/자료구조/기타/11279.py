45a8d06d000121560fe7b81b81316f78d863b3b3
homerunbaII <thomas3737@korea.ac.kr>
2023-03-15 19:27:02 +0900

    0315

---
+++ python/알고리즘/자료구조/기타/11279.py
@@ -0,0 +1,32 @@
+# from queue import PriorityQueue
+
+# n = int(input())
+
+# que = PriorityQueue(maxsize=100000)
+
+# for i in range(n):
+#     x = int(input())
+#     if x == 0:
+#         if que.empty() == True:
+#             print(0)
+#         else:
+#             print(que.get()[1])
+#     else:
+#         x_sort = (-x, x)
+#         que.put(x_sort)]
+import sys
+from heapq import *
+
+heap = []
+
+n = int(input())
+
+for i in range(n):
+    x = int(sys.stdin.readline)
+    if x == 0:
+        if heap:
+            print(-heappop(heap))
+        else:
+            print(0)
+    else:
+        heappush(heap, -x)
