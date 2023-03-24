9bd0cf3a1dab81a31c12d8367101d41ff1882f0d
homerunbaII <thomas3737@korea.ac.kr>
2023-03-24 11:08:11 +0900

    0324

--- python/알고리즘/동적 계획법/2748.py
+++ python/알고리즘/동적 계획법/2748.py
@@ -1,14 +1,12 @@
-
 fibo_list = [0, 1]
 
-
-n = int(input()) + 1
-if n == 1:
+n = int(input())
+if n == 0:
     print(0)
-elif n == 2:
+elif n == 1:
     print(1)
 else:
-    for i in range(2, n):
+    for i in range(2, n+1):
         result = fibo_list[i-1] + fibo_list[i-2]
-        fibo_list.append(result)
+        fibo_list[i] = (result)
     print(fibo_list[-1])
--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-i = 0
-num = 5
-n = 8
-while (num - i > 0 and num + i < n):
-    i += 1
+for i in range(10, 0, -1):
     print(i)
