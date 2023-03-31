855c6418173a24e54bd555c9cb869d14f89f0ec6
homerunbaII <thomas3737@korea.ac.kr>
2023-03-31 11:08:49 +0900

    0331

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ n = int(input())
 r = 0
 
 for i in range(1, n):
-    for j in range(i+1, n):
+    print('i', i,)
+    for j in range(i+1, n + 1):
+        print('j', j, end=' ')
         for k in range(1, j+1):
+            print(k, end='')
             r = r+1
+        print()
 
 print(r)
