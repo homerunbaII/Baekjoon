de9e2d6265108f86766f127ddc595a92bd25f2b4
homerunbaII <thomas3737@korea.ac.kr>
2023-04-08 06:55:49 +0900

    0408

---
+++ python/알고리즘/동적 계획법/2156.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+grape = []
+dp = [0 for _ in range(n+2)]
+
+for i in range(n):
+    grape.append(int(input()))
+
+grape.append(0)
+grape.append(0)
+
+dp[0] = grape[0]
+dp[1] = grape[0] + grape[1]
+dp[2] = max(grape[0] + grape[1], grape[1] + grape[2])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-2] + grape[i], dp[i - 3] + grape[i-1] + grape[i], dp[i-1])
+
+
+print(max(dp))
