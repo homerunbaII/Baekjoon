2de1d9220075037e19a578aec187f47fd4d043bd
homerunbaII <thomas3737@korea.ac.kr>
2023-04-10 19:45:12 +0900

    0410

--- python/알고리즘/동적 계획법/2156.py
+++ python/알고리즘/동적 계획법/2156.py
@@ -14,7 +14,7 @@ grape.append(0)
 
 dp[0] = grape[0]
 dp[1] = grape[0] + grape[1]
-dp[2] = max(grape[0] + grape[1], grape[1] + grape[2])
+dp[2] = max(grape[0] + grape[1], grape[1] + grape[2], grape[0] + grape[2])
 
 for i in range(3, n):
     dp[i] = max(dp[i-2] + grape[i], dp[i - 3] + grape[i-1] + grape[i], dp[i-1])
