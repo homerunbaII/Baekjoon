0bcb8363a7bf9886e9abe2a065cb971098a8e313
homerunbaII <thomas3737@korea.ac.kr>
2023-04-18 16:25:38 +0900

    0418

---
+++ python/알고리즘/구현/17413.py
@@ -0,0 +1,30 @@
+char = input()
+new_char = ''
+temp_char = ''
+
+flag = 0
+gap_flag = 0
+
+for i in range(len(char)):
+    if flag == 0 and char[i] == ' ':
+        new_char += temp_char[::-1]
+        temp_char = ''
+    if char[i] == '<':
+        flag = 1
+        new_char += temp_char[::-1]
+        temp_char = ''
+    if flag == 1:
+        new_char += char[i]
+    if flag == 0:
+        if char[i] == ' ':
+            new_char += ' '
+        else:
+            temp_char += char[i]
+    if char[i] == '>':
+        flag = 0
+    print(temp_char)
+
+new_char += temp_char[::-1]
+
+
+print(new_char)
