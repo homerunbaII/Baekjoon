edcacea8111eb6b54a704e4d3230327754a85331
homerunbaII <thomas3737@korea.ac.kr>
2023-04-19 21:48:16 +0900

    0419

---
+++ python/알고리즘/구현/22858.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+s = list(map(int, input().split()))
+d = list(map(int, input().split()))
+
+p = [0 for _ in range(n)]
+
+
+for _ in range(k):
+    for j in range(n):
+        p[d[j]-1] = s[j]
+    s = list(p)
+
+
+print(*p)
