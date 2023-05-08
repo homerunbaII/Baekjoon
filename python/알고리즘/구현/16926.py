a843b9c1726b94fe7706307c27348b564664a4d7
homerunbaII <thomas3737@korea.ac.kr>
2023-05-08 23:59:18 +0900

    0508

--- python/알고리즘/구현/16926.py
+++ python/알고리즘/구현/16926.py
@@ -5,4 +5,29 @@ arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-print(arr)
+for k in range(r):
+    s = min(n, m) // 2
+    for j in range(s):
+        x, y = j, j
+        pre = arr[x][y]
+        for i in range(x + 1, n - j):
+            tmp = arr[i][j]
+            arr[i][j] = pre
+            pre = tmp
+        for i in range(y+1, m - j):
+            tmp = arr[n - 1 - j][i]
+            arr[n - 1 - j][i] = pre
+            pre = tmp
+        for i in range(n-j-2, j - 1, -1):
+            tmp = arr[i][m-1-j]
+            arr[i][m-1-j] = pre
+            pre = tmp
+        for i in range(m-2-j, j-1, -1):
+            tmp = arr[j][i]
+            arr[j][i] = pre
+            pre = tmp
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j], end=' ')
+    print()
