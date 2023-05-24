55dcc1072e3e08c57174154ae0dd8dba7c236591
homerunbaII <thomas3737@korea.ac.kr>
2023-05-24 23:13:42 +0900

    0524

---
+++ python/알고리즘/그리디/11000.py
@@ -0,0 +1,7 @@
+n = int(input())
+time_list = []
+
+for i in range(n):
+    time_list.append(list(map(int, input().split())))
+
+print(time_list)
--- test3.py
+++ test3.py
@@ -1,4 +1,20 @@
-b = 12
-b //= 2
+import sys
 
-print(b)
+input = sys.stdin.readline
+
+n = int(input())
+meetlist = []
+
+for i in range(n):
+    meetlist.append(list(map(int, input().split())))
+
+meetlist.sort(key=lambda x: (x[1], x[0]))
+cnt = 1
+endtime = meetlist[0][1]  # 4
+
+for i in meetlist:
+    if endtime <= i[0]:
+        cnt += 1
+        endtime = i[1]
+
+print(cnt)
