e95c958a97976ff91fef24786eb6b18bacdf6fff
homerunbaII <thomas3737@korea.ac.kr>
2023-05-26 14:35:00 +0900

    0526

---
+++ python/알고리즘/그리디/2798.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+arr = list(map(int, input().split()))
+maximum = 0
+for i in range(n-2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            temp = arr[i] + arr[j] + arr[k]
+            if temp > m:
+                temp = 0
+            if temp > maximum:
+                maximum = temp
+
+
+print(maximum)
