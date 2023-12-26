f64623850e5788f557e495e8389027f433cbe13e
homerunbaII <thomas3737@korea.ac.kr>
2023-12-26 17:32:00 +0900

    10431

---
+++ python/알고리즘/구현/10431.py
@@ -0,0 +1,14 @@
+import sys
+
+p = int(input())
+
+for i in range(p):
+    test_list = list(map(int, sys.stdin.readline().split()))
+    step = 0
+    for i in range(1,21):
+        for j in range(i + 1,21):
+            
+            if test_list[i] > test_list[j]:
+                step +=1
+                print(test_list[i], test_list[j], step)
+    print(test_list[0], step)
