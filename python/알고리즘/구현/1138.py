e93818f92f8dc58b1105c3148cdcaa5428cbbcd7
homerunbaII <thomas3737@korea.ac.kr>
2023-12-29 15:01:35 +0900

    1138

---
+++ python/알고리즘/구현/1138.py
@@ -0,0 +1,24 @@
+n = int(input())
+height_list = list(map(int, input().split()))
+order_list = [0] * 10
+# 5 3 2 1 4
+# 5
+# 1 2 3 4 5
+# 3 2 1 1 0
+
+# i 는 1부터이다
+for i in range(1, n + 1):
+    checker = 0
+    for j in range(n):
+        if order_list[j] != 0:
+            continue
+        if checker == height_list[i - 1]:
+            if order_list[j] == 0:
+                order_list[j] = i
+                break
+            else: continue
+        if i > order_list[j] :
+            checker += 1
+
+for i in range(n):
+    print(order_list[i], end = ' ')
