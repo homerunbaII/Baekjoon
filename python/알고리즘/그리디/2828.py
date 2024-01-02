c14aa9209079cafbd44f675c9cf4513d0457d2f6
homerunbaII <thomas3737@korea.ac.kr>
2024-01-02 20:59:31 +0900

    2828

---
+++ python/알고리즘/그리디/2828.py
@@ -0,0 +1,27 @@
+screen , basket = map(int, input().split())
+apple = int(input())
+
+basket_location = [i + 1 for i in range(basket)]
+distance = 0
+print(basket_location)
+
+for i in range(apple):
+    apple_location = int(input())
+    if apple_location in basket_location:
+        continue
+    elif apple_location < basket_location[0]:
+        while apple_location not in basket_location:
+            for j in range(len(basket_location)):
+                basket_location[j] -= 1
+            distance += 1
+        continue
+    elif apple_location > basket_location[-1]:
+        while apple_location not in basket_location:
+            for j in range(len(basket_location)):
+                basket_location[j] += 1
+            distance += 1
+        continue
+
+print(distance)
+
+
