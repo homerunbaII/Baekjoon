9719178077b78d6aeba9d14ee4548ceefc05448b
homerunbaII <thomas3737@korea.ac.kr>
2024-01-15 00:07:34 +0900

    2503

--- python/알고리즘/브루트 포스/2503.py
+++ python/알고리즘/브루트 포스/2503.py
@@ -1,37 +1,23 @@
 from itertools import permutations
 
 n = int(input())
-
-guess_list = []
-all_list = [i for i in range(100,1000)]
+number_list = [str(i) for i in range(1,10)]
+all = list(permutations(number_list, 3))
 
 for i in range(n):
     guess, strike ,ball = map(int, input().split())
-    guess = str(guess)
-    num_100 = int(guess[0])
-    num_10 = int(guess[1])
-    num_1 = int(guess[2])
-    guessnum_list= [num_100, num_10, num_1]
-    number_list = [i for i in range(1,10)]
-    if strike == 0 and ball == 0:
-        for i in guessnum_list:
-            number_list.remove(i)
-        data = permutations(number_list, 3)
-        for i in data:
-            all_list.remove(i[0] * 100 + i[1] * 10 + i[2])
-    if strike == 1 and ball == 0:
-        pass
-    if strike == 0 and ball == 1:
-        pass
-    if strike == 1 and ball == 1:
-        pass
-    if strike == 2 and ball == 0:
-        pass
-    if strike == 0 and ball == 2:
-        pass
-    if strike == 1 and ball == 2:
-        pass
-    if strike == 2 and ball == 1:
-        pass
+    guess = list(str(guess))
+    remove_cnt = 0
+    for i in range(len(all)):
+        s, b = 0, 0
+        i -= remove_cnt
+        for j in range(3):
+            if all[i][j] == guess[j]:
+                s += 1
+            elif guess[j] in all[i]:
+                b += 1
+        if  s != strike or b != ball:
+            all.remove(all[i])
+            remove_cnt += 1
 
-print(all_list)
+print(len(all))
