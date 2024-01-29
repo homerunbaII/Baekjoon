fa8c6344aacfa3c7f56f748ecde512d591c94487
homerunbaII <thomas3737@korea.ac.kr>
2024-01-29 18:39:31 +0900

    4949

--- python/알고리즘/정렬/2587.py
+++ python/알고리즘/정렬/2587.py
@@ -1,9 +1,5 @@
-num_list = []
+answer = "123"
+answer += "123"
 
-for i in range(5):
-    num_list.append(int(input()))
-
-sorted_list = sorted(num_list)
-
-print(int(sum(sorted_list) / 5))
-print(sorted_list[2])
+print(answer)
+    
