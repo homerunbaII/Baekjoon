64ccf3ba7f48380f59ef2cd71b355bf258e0983b
homerunbaII <thomas3737@korea.ac.kr>
2024-02-07 22:03:42 +0900

    1932

---
+++ python/알고리즘/동적 계획법/1932.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+dp = [0 for _ in range(n)]
+
+for i in range(n):
+    dp[i] = list(map(int, input().split()))
+
+dp[1][0] += dp[0][0]
+dp[1][1] += dp[0][0]
+
+for i in range(2, n):
+    for j in range(i + 1):
+        if j == 0:
+            dp[i][j] += dp[i -1][j]
+        elif j == i :
+            dp[i][j] += dp[i -1][j - 1]
+        else:        
+            dp[i][j] += max(dp[i -1][j - 1], dp[i - 1][j])
+
+print(max(dp[n-1]))
