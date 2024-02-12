1faf46a6e5cdd4a2d8c135acddac48dd6d008586
homerunbaII <thoma3737@korea.ac.kr>
2024-02-12 23:31:52 +0900

    2529

---
+++ python/알고리즘/백트래킹/2529.py
@@ -0,0 +1,39 @@
+n = int(input())
+# < > < >
+operation = list(input().split())
+# 0 1 2 3 4 5 6 7 8 9
+visited = [0 for _ in range(10)]
+result = []
+
+def dfs(cnt,lst, before):
+    if cnt == n + 1:
+        lst = list(map(str, lst))
+        lst = ''.join(lst)
+        result.append(lst)
+    else:
+        if cnt == 0:
+                for i in range(0, 10):
+                    visited[i] = 1
+                    dfs(cnt + 1,lst + [i], i)
+                    visited[i] = 0
+        else:
+            for i in range(0, 10):
+                if visited[i] == 0:
+                    if operation[cnt - 1] == '>': 
+                        if before > i:
+                            visited[i] = 1
+                            dfs(cnt + 1,lst + [i], i)
+                            visited[i] = 0 
+                    elif operation[cnt - 1] == '<':
+                        if before < i:
+                            visited[i] = 1
+                            dfs(cnt + 1,lst + [i], i)
+                            visited[i] = 0
+                    
+
+dfs(0,[],0)
+k = max(result)
+
+print(f'{k:012}')
+print(max(result))
+print(min(result))
