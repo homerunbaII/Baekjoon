4f0bc79c00cdd45007733c89597f26ed956dafdc
homerunbaII <thomas3737@korea.ac.kr>
2024-02-14 00:06:01 +0900

    1699

--- python/알고리즘/동적 계획법/1699.py
+++ python/알고리즘/동적 계획법/1699.py
@@ -1,22 +1,13 @@
-import math
-
 n = int(input())
+dp = [i for i in range(n + 1)]
 
-dp = [10000] * 100001
-dp[0] = 0
-square_list = []
+for i in range(2, n + 1):
+    for j in range(1, i + 1):
+        s = j ** 2
+        if s > i : 
+            break 
+        if dp[i] > dp[i - s] + 1 :
+            dp[i] = dp[i - s] + 1
 
-for i in range(1, 100001):
-    temp = int(math.sqrt(i))
-    if temp ** 2 == i:
-        square_list.append(i)
-
-# for i in square_list:
-#     dp[i] = dp[i] + dp[i-1]
-
-for i in range(1, 100001):
-    for j in square_list:
-        if j  <= i:
-            if dp[i] > dp[i - j] + 1 :
-                dp[i] = dp[i - j] + 1
 print(dp[n])
+print(dp)
