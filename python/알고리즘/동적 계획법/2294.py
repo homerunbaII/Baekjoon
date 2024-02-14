50c5dc93646a311dc800c6c9412a4f766ffc21c4
homerunbaII <thomas3737@korea.ac.kr>
2024-02-14 20:32:27 +0900

    2294

---
+++ python/알고리즘/동적 계획법/2294.py
@@ -0,0 +1,27 @@
+n , target = map(int, input().split())
+
+coin_list = []
+dp = [10E9] * 10001
+dp[0] = 0
+
+for _ in range(n):
+    coin = int(input())
+    coin_list.append(coin)
+
+coin_list = list(set(coin_list))
+coin_list.sort()
+
+for i in coin_list:
+    n = i 
+    while n <= 10000:
+        if dp[n] > dp[n-i] + 1:
+            dp[n] =  dp[n-i] + 1
+        n += 1
+
+
+print(dp)
+
+if dp[target] == 10E9:
+    print(-1)
+else: 
+    print(dp[target])
