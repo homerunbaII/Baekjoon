5d92afd90f7ebf2fadf42aa188d922ad908873d7
homerunbaII <thomas3737@korea.ac.kr>
2024-02-15 17:09:42 +0900

    15486

--- python/알고리즘/동적 계획법/11722.py
+++ python/알고리즘/동적 계획법/11722.py
@@ -6,7 +6,6 @@ num_list = num_list[:: -1]
 ## 10 20 20 10 30 10 자기보다 작은 수들 중의 dp[max]
 dp = [1 for _ in range(n)] 
 
-print(num_list)
 for i in range(n):
     for j in range(0, i):
         if num_list[i] > num_list[j]:
---
+++ python/알고리즘/동적 계획법/15486.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0 for _ in range(n + 1)]
+max_dp = 0
+for i in range(n):
+    day, cost = map(int,input().split())
+    if dp[i] > max_dp:
+        max_dp = dp[i]
+    if dp[i] < max_dp:
+        dp[i] = max_dp
+    if (i + day) <= n:
+        if dp[i +day] < dp[i] + cost :
+            dp[i + day] = dp[i] + cost
+
+print(dp[n])
