3f9fdcc74a7d8bddbf8b88ff6db9fe6dd7363992
homerunbaII <thomas3737@korea.ac.kr>
2024-02-18 16:54:11 +0900

    11066

--- python/알고리즘/동적 계획법/11066.py
+++ python/알고리즘/동적 계획법/11066.py
@@ -1,20 +1,60 @@
-import heapq as h
+from itertools import combinations, permutations
 
-for _ in range(int(input())):
-    n = int(input())
-    file = list(map(int, input().split()))
-    files = []
-    total_cost = 0
-    ## 최소 힙 
-    for i in file:
-        h.heappush(files, i)
-    print(files)
-    while len(files) > 1:
-        file_1 = h.heappop(files)
-        file_2 = h.heappop(files)
-        new_file = file_1 + file_2
-        print(file_1, file_2 ,new_file)
-        h.heappush(files, new_file)
-        total_cost += new_file
-    print(files, total_cost)
+n = int(input())
+power_list = [list(map(int, input().split())) for _ in range(n)]
+
+# 각 팀원
+team_person = n // 2
+# 선수 번호 0 ~ n -1
+player = [i for i in range(n)]
+# 선수 번호는 0번부터 시작
+team = list(combinations(range(1,n), team_person - 1))
+
+# 팀 1
+start_team = [[0] for _ in range(len(team))]
+# 팀 2
+link_team = []
+
+for i in range(len(start_team)):
+    start_team[i] = [0] + list(team[i])
+
+# 팀 1, 팀 2 케이스 구하기
+
+for i in start_team:
+    temp = []
+    for j in player:
+        if j not in i:
+            temp.append(j)
+    link_team.append(temp)
+            
+start_team_power = []
+link_team_power = []
+
+# 각각 힘의 합 구하기
+
+for i in start_team:
+    power = 0
+    temp = list(permutations(i, 2))
+    for j in temp :
+        x, y = j
+        power += power_list[x][y]
+    start_team_power.append(power)
+    print(power)
+
+for i in link_team:
+    power = 0
+    temp = list(permutations(i ,2))
+    for j in temp :
+        x, y = j
+        power += power_list[x][y]
+    link_team_power.append(power)
+
+## 힘 차이 구하기
+power_difference = []
+
+for i in range(len(start_team_power)):
+    power_difference.append(abs(start_team_power[i] - link_team_power[i]))
+
+## 최소값 출력
+print(min(power_difference))
     
