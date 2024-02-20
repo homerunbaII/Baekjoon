7c568fcb13f5a7985da97c124852ca028145f451
homerunbaII <thomas3737@korea.ac.kr>
2024-02-20 21:25:48 +0900

    11052

---
+++ python/알고리즘/동적 계획법/11052.py
@@ -0,0 +1,17 @@
+n = int(input())
+pack = list(map(int, input().split()))
+dp = [0] * (n+1)
+card = [0] *(n+1)
+for i in range(n):
+    card[i + 1] = pack[i]
+
+for i in range(1, n+1):
+    ## 1번 팩
+    card_now = card[i]
+
+    for j in range(i, n + 1):
+
+        if dp[j] < dp[j - i] + card_now:
+            dp[j] = dp[j - i] + card_now
+
+print(dp[n])
