a76a20a27569ba3c894ed0761a5d69b7def79ed8
homerunbaII <thomas3737@korea.ac.kr>
2024-02-28 21:52:34 +0900

    1806:

---
+++ python/알고리즘/투포인터/1806.py
@@ -0,0 +1,25 @@
+n, s = map(int, input().split())
+n_list = list(map(int, input().split()))
+
+small = 10e9
+
+p1, p2 = 0,0
+total = 0
+
+while p2 < n :
+    if total >= s :
+        small = min(p2 - p1, small)
+        total -= n_list[p1]
+        p1 += 1
+    else:
+        total += n_list[p2]
+        p2 += 1
+
+if small == 10e9:
+    print(0)
+else:
+    print(small)
+        
+
+
+    
