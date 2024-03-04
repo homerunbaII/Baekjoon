004b66e6213303732cd46eb612e96042cd44c671
homerunbaII <thomas3737@korea.ac.kr>
2024-03-04 11:27:49 +0900

    9461

---
+++ python/알고리즘/동적 계획법/9461.py
@@ -0,0 +1,12 @@
+
+
+dp = [0 for _ in range(101)]
+dp[1], dp[2], dp[3], dp[4], dp[5] = 1, 1, 1, 2, 2
+dp[6], dp[7], dp[8], dp[9], dp[10] = 3, 4, 5, 7, 9
+
+for i in range(11, 101):
+    dp[i] = dp[i-1] + dp[i-5]
+
+for i in range(int(input())):
+    n = int(input())
+    print(dp[n])
