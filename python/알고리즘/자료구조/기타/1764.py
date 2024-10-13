639cbed6af7982567e569c719b3d1bcdf3df5e08
김현수 <thomas3737@korea.ac.kr>
2024-10-13 12:10:32 +0900

    1764

---
+++ python/알고리즘/자료구조/기타/1764.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n , m = map(int, input().split())
+
+
+d_dict = {}
+b_dict = {}
+dbj = []
+
+for i in range(n):
+    name = input().rstrip()
+    d_dict[name] = 1
+
+for i in range(m):
+    name = input().rstrip()
+    b_dict[name] = 1
+
+for i in d_dict:
+    if i in b_dict:
+        dbj.append(i)
+
+dbj.sort()
+print(dbj)
